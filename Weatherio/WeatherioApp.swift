aa32d85a444a4014bfc46d084097ab065ef8fe6e
refriansyari <refriansyari@gmail.com>
2025-04-07 17:11:01 +0700

    Initial Commit

---
+++ Weatherio/WeatherioApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherioApp.swift
+//  Weatherio
+//
+//  Created by Refri Ansyari on 07/04/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherioTests/WeatherioTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherioTests.swift
+//  WeatherioTests
+//
+//  Created by Refri Ansyari on 07/04/25.
+//
+
+import Testing
+@testable import Weatherio
+
+struct WeatherioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
